6d465df8d0483932423e801c3d4a894603058df8
Stackbit <projects@stackbit.com>
2021-02-05 19:24:04 +0000

    initial commit by Stackbit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import HeroBanner from '../components/HeroBanner.svelte';
+  import Overview from '../components/Overview.svelte';
+  import OpenHours from '../components/OpenHours.svelte';
+</script>
+
+<style>
+
+</style>
+
+<svelte:head>
+  <title>NerdCafé</title>
+</svelte:head>
+
+<div>
+  <HeroBanner
+    bannerTitle="Welcome"
+    bannerSubtitle="You should come and taste for yourself"
+    themeBannerFullHeight
+    bannerImg="homepage" />
+  <Overview />
+  <OpenHours />
+</div>
